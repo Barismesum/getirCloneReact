37ebee77503999511d8a0c8d190645e380749ab1
Baris Mesum <bmesum99@gmail.com>
2022-10-25 14:31:43 +0300

    herosection ve header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "components/Header";
+import HeroSection from "components/HeroSection";
+import Favorites from "components/Favorites";
+import Campaigns from "components/Campaigns";
+import Card from "components/Card";
+import Footer from "components/Footer";
+import MobileApp from "components/MobileApp";
+import Categories from "components/Categories";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <HeroSection />
+      <Categories />
+      <Campaigns />
+      <Favorites />
+      <MobileApp />
+      <Card />
+      <Footer />
+    </>
   );
 }
 
